d8cdd21b1c9d3ccd675fd625d4ef0cd7167b41e2
amirisback <faisalamircs@gmail.com>
2019-07-14 06:40:37 +0700

    Initial commit

---
+++ app/src/main/java/com/frogobox/faisalamirprofile/SocialMediaActivity.kt
@@ -0,0 +1,12 @@
+package com.frogobox.faisalamirprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SocialMediaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_social_media)
+    }
+}
