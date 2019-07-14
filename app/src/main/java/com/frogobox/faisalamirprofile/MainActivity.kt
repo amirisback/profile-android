1f4528f7f5d55d6fe30ae598347b72da91723f85
amirisback <faisalamircs@gmail.com>
2019-07-14 08:05:29 +0700

    add: drawable content

--- app/src/main/java/com/frogobox/faisalamirprofile/MainActivity.kt
+++ app/src/main/java/com/frogobox/faisalamirprofile/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.frogobox.faisalamirprofile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        fab_skill.setOnClickListener {
+            val intentSkill = Intent(this, SkillActivity::class.java)
+            startActivity(intentSkill)
+        }
+
+        fab_certificate.setOnClickListener {
+            val intentCertificate = Intent(this, CertificateActivity::class.java)
+            startActivity(intentCertificate)
+        }
+
+        fab_social_media.setOnClickListener {
+            val intentSocialMedia = Intent(this, SocialMediaActivity::class.java)
+            startActivity(intentSocialMedia)
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/frogobox/faisalamirprofile/SkillActivity.kt
@@ -0,0 +1,12 @@
+package com.frogobox.faisalamirprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+}
