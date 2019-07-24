3dcb574febf03ed91e1237574fcc4e72e69b5ad9
amirisback <faisalamircs@gmail.com>
2019-07-24 23:34:26 +0700

    add: NFC Function

--- app/src/main/java/com/frogobox/faisalamirprofile/MainActivity.kt
+++ app/src/main/java/com/frogobox/faisalamirprofile/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.frogobox.faisalamirprofile
 
 import android.content.Intent
+import android.nfc.NfcAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.frogobox.faisalamirprofile.helper.NFCUtil
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var mNfcAdapter: NfcAdapter? = null
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this)
 
         fab_skill.setOnClickListener {
             val intentSkill = Intent(this, SkillActivity::class.java)
@@ -29,4 +34,30 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+
+    override fun onResume() {
+        super.onResume()
+        mNfcAdapter?.let {
+            NFCUtil.enableNFCInForeground(it, this, javaClass)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mNfcAdapter?.let {
+            NFCUtil.disableNFCInForeground(it, this)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        val messageWrittenSuccessfully = NFCUtil.createNFCMessage("Muhammad Faisal Amir", intent)
+        if (messageWrittenSuccessfully) {
+            Toast.makeText(this, "Successful Written to Tag", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "Something When wrong Try Again", Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
