cbb9187be94ffae6231009742a43d74e3a05cceb
amirisback <faisalamircs@gmail.com>
2019-11-09 22:54:06 +0700

    update: Product, Education Fragment

--- app/src/main/java/com/frogobox/faisalamirprofile/fragment/EducationFragment.kt
+++ app/src/main/java/com/frogobox/faisalamirprofile/fragment/EducationFragment.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.frogobox.faisalamirprofile.R
+import com.frogobox.faisalamirprofile.adapter.EducationAdapter
 import com.frogobox.faisalamirprofile.base.BaseFragment
+import com.frogobox.faisalamirprofile.model.Education
+import kotlinx.android.synthetic.main.fragment_education.*
 
 
 class EducationFragment : BaseFragment() {
@@ -19,5 +24,42 @@ class EducationFragment : BaseFragment() {
         return inflater.inflate(R.layout.fragment_education, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListView()
+    }
+
+    private fun initListView() {
+        val arrayEducation: MutableList<Education> = mutableListOf()
+
+        arrayEducation.add(
+            Education(
+                R.drawable.img_logo_smasa,
+                getString(R.string.text_edu_high_school_level),
+                getString(
+                    R.string.text_edu_high_school_institute
+                ),
+                "",
+                getString(R.string.text_edu_high_school_year)
+            )
+        )
+        arrayEducation.add(
+            Education(
+                R.drawable.img_logo_telu,
+                getString(R.string.text_edu_diploma_level),
+                getString(
+                    R.string.text_edu_diploma_institute
+                ),
+                getString(R.string.text_edu_diploma_major),
+                getString(R.string.text_edu_diploma_year)
+            )
+        )
+
+        val adapter = EducationAdapter(context, arrayEducation)
+        val mLayoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+
+        rv_education.adapter = adapter
+        rv_education.layoutManager = mLayoutManager
+    }
 
 }
--- app/src/main/java/com/frogobox/faisalamirprofile/fragment/ProductFragment.kt
+++ app/src/main/java/com/frogobox/faisalamirprofile/fragment/ProductFragment.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.frogobox.faisalamirprofile.R
+import com.frogobox.faisalamirprofile.adapter.ProductAdapter
 import com.frogobox.faisalamirprofile.base.BaseFragment
+import com.frogobox.faisalamirprofile.model.Product
+import kotlinx.android.synthetic.main.fragment_product.*
 
 
 class ProductFragment : BaseFragment() {
@@ -19,5 +24,80 @@ class ProductFragment : BaseFragment() {
         return inflater.inflate(R.layout.fragment_product, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListView()
+    }
+
+    private fun initListView(){
+        val arrayProduct : MutableList<Product> = mutableListOf()
+
+        arrayProduct.add(
+            Product(getString(R.string.product_name_cat),
+                R.drawable.ic_product_cat,
+                R.drawable.ic_product_type_android
+            )
+        )
+        arrayProduct.add(
+            Product(getString(R.string.product_name_finpro),
+                R.drawable.ic_product_finpro,
+                R.drawable.ic_product_type_android
+            )
+        )
+        arrayProduct.add(
+            Product(getString(R.string.product_name_football),
+                R.drawable.ic_product_football,
+                R.drawable.ic_product_type_android
+            )
+        )
+        arrayProduct.add(
+            Product(getString(R.string.product_name_frogonews),
+                R.drawable.ic_product_frogonews,
+                R.drawable.ic_product_type_android
+            )
+        )
+        arrayProduct.add(
+            Product(getString(R.string.product_name_jagosholat),
+                R.drawable.ic_product_jagosholat,
+                R.drawable.ic_product_type_android
+            )
+        )
+        arrayProduct.add(
+            Product(getString(R.string.product_name_jami),
+                R.drawable.ic_product_jami,
+                R.drawable.ic_product_type_android
+            )
+        )
+        arrayProduct.add(
+            Product(getString(R.string.product_name_movie),
+                R.drawable.ic_product_movie,
+                R.drawable.ic_product_type_android
+            )
+        )
+        arrayProduct.add(
+            Product(getString(R.string.product_name_mood),
+                R.drawable.ic_product_mood,
+                R.drawable.ic_product_type_android
+            )
+        )
+        arrayProduct.add(
+            Product(getString(R.string.product_name_romis),
+                R.drawable.ic_product_romis,
+                R.drawable.ic_product_type_android
+            )
+        )
+        arrayProduct.add(
+            Product(getString(R.string.product_name_shejek),
+                R.drawable.ic_product_shejek,
+                R.drawable.ic_product_type_android
+            )
+        )
+
+        val adapter = ProductAdapter(context, arrayProduct)
+        val mLayoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL ,false)
+
+        rv_product.adapter = adapter
+        rv_product.layoutManager = mLayoutManager
+    }
 
 }
