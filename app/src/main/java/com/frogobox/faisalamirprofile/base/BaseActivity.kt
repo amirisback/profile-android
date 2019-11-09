a73a3bb432aa2e8895ac247bd118ffcc7229bc2a
amirisback <faisalamircs@gmail.com>
2019-11-09 22:14:36 +0700

    add: Base Code Kotlin

--- app/src/main/java/com/frogobox/faisalamirprofile/base/BaseActivity.kt
+++ app/src/main/java/com/frogobox/faisalamirprofile/base/BaseActivity.kt
@@ -1,45 +1,16 @@
 package com.frogobox.faisalamirprofile.base
 
-import android.annotation.SuppressLint
-import android.app.*
-import android.app.assist.AssistContent
-import android.content.*
-import android.content.pm.ApplicationInfo
-import android.content.pm.PackageManager
-import android.content.res.AssetManager
-import android.content.res.Configuration
-import android.content.res.Resources
-import android.database.Cursor
-import android.database.DatabaseErrorHandler
-import android.database.sqlite.SQLiteDatabase
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.drawable.Drawable
-import android.net.Uri
-import android.os.*
-import android.transition.Scene
-import android.transition.TransitionManager
-import android.util.AttributeSet
+import android.content.Intent
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.os.Handler
 import android.view.*
-import android.view.accessibility.AccessibilityEvent
-import androidx.activity.OnBackPressedCallback
-import androidx.appcompat.app.ActionBar
-import androidx.appcompat.app.ActionBarDrawerToggle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.view.ActionMode
-import androidx.appcompat.widget.Toolbar
-import androidx.core.app.SharedElementCallback
-import androidx.core.app.TaskStackBuilder
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.ViewModelStore
-import androidx.loader.app.LoaderManager
-import java.io.*
-import java.util.concurrent.Executor
-import java.util.function.Consumer
+import com.frogobox.faisalamirprofile.R
+import com.frogobox.faisalamirprofile.helper.ConstHelper
 
 /**
  * Created by Faisal Amir
@@ -60,5 +31,130 @@ import java.util.function.Consumer
  */
 
 open class BaseActivity : AppCompatActivity() {
+
+    lateinit var mActivity: AppCompatActivity
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mActivity = this
+    }
+
+    protected fun setupCustomTitleToolbar(title: Int) {
+        supportActionBar?.setTitle(title)
+    }
+
+    protected fun setupNoLimitStatBar() {
+        val windows = window // in Activity's onCreate() for instance
+        windows.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+    }
+
+    protected inline fun <reified ClassActivity> setupSplashScreen() {
+        Handler().postDelayed(object : Runnable {
+            override fun run() {
+                baseStartActivity<ClassActivity>()
+                this@BaseActivity.finish()
+            }
+
+        }, ConstHelper.Const.SPLASH_INTERVAL.toLong())
+    }
+
+    protected fun setupFullScreen() {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+    }
+
+    protected fun setupChildFragment(frameId: Int, fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(frameId, fragment)
+            commit()
+        }
+    }
+
+    protected inline fun <reified ClassActivity> baseStartActivity() {
+        this.startActivity(Intent(this, ClassActivity::class.java))
+    }
+
+    protected inline fun <reified ClassActivity, Model> baseStartActivity(
+        extraKey: String,
+        data: Model
+    ) {
+        val intent = Intent(this, ClassActivity::class.java)
+        val extraData = BaseHelper().baseToJson(data)
+        intent.putExtra(extraKey, extraData)
+        this.startActivity(intent)
+    }
+
+    protected inline fun <reified Model> baseGetExtraData(extraKey: String): Model {
+        val extraIntent = intent.getStringExtra(extraKey)
+        val extraData = BaseHelper().baseFromJson<Model>(extraIntent)
+        return extraData
+    }
+
+    protected fun checkExtra(extraKey: String): Boolean {
+        return intent?.hasExtra(extraKey)!!
+    }
+
+    protected fun <Model> baseFragmentNewInstance(
+        fragment: BaseFragment,
+        argumentKey: String,
+        extraDataResult: Model
+    ) {
+        fragment.baseNewInstance(argumentKey, extraDataResult)
+    }
+
+    protected fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    protected fun setupDetailActivity(title: String) {
+        setTitle(title)
+        val upArrow = ContextCompat.getDrawable(this, R.drawable.ic_toolbar_back_home)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(upArrow)
+        supportActionBar?.setBackgroundDrawable(
+            ColorDrawable(
+                ContextCompat.getColor(
+                    this,
+                    R.color.colorBaseWhite
+                )
+            )
+        )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    protected fun setupEventEmptyView(view: View, isEmpty: Boolean) {
+        if (isEmpty) {
+            view.visibility = View.VISIBLE
+        } else {
+            view.visibility = View.GONE
+        }
+    }
+
+    protected fun setupEventProgressView(view: View, progress: Boolean) {
+        if (progress) {
+            view.visibility = View.VISIBLE
+        } else {
+            view.visibility = View.GONE
+        }
+    }
 
 }
--- app/src/main/java/com/frogobox/faisalamirprofile/base/BaseFragment.kt
+++ app/src/main/java/com/frogobox/faisalamirprofile/base/BaseFragment.kt
@@ -1,5 +1,9 @@
 package com.frogobox.faisalamirprofile.base
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 /**
@@ -19,4 +23,72 @@ import androidx.fragment.app.Fragment
  * com.frogobox.faisalamirprofile.base
  *
  */
-class BaseFragment : Fragment() {}
+class BaseFragment : Fragment() {
+
+    lateinit var mActivity: BaseActivity
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mActivity = (activity as BaseActivity)
+    }
+
+    protected fun setupChildFragment(frameId: Int, fragment: Fragment) {
+        childFragmentManager.beginTransaction().apply {
+            replace(frameId, fragment)
+            commit()
+        }
+    }
+
+    fun <Model> baseNewInstance(argsKey: String, data: Model) {
+        val argsData = BaseHelper().baseToJson(data)
+        val bundleArgs = Bundle().apply {
+            putString(argsKey, argsData)
+        }
+        this.arguments = bundleArgs
+    }
+
+    protected inline fun <reified Model> baseGetInstance(argsKey: String): Model {
+        val argsData = this.arguments?.getString(argsKey)
+        val instaceData = BaseHelper().baseFromJson<Model>(argsData)
+        return instaceData
+    }
+
+    protected fun checkArgument(argsKey: String): Boolean {
+        return arguments!!.containsKey(argsKey)
+    }
+
+    protected fun setupEventEmptyView(view: View, isEmpty: Boolean) {
+        if (isEmpty) {
+            view.visibility = View.VISIBLE
+        } else {
+            view.visibility = View.GONE
+        }
+    }
+
+    protected fun setupEventProgressView(view: View, progress: Boolean) {
+        if (progress) {
+            view.visibility = View.VISIBLE
+        } else {
+            view.visibility = View.GONE
+        }
+    }
+
+    protected fun showToast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    protected inline fun <reified ClassActivity> baseStartActivity() {
+        context?.startActivity(Intent(context, ClassActivity::class.java))
+    }
+
+    protected inline fun <reified ClassActivity, Model> baseStartActivity(
+        extraKey: String,
+        data: Model
+    ) {
+        val intent = Intent(context, ClassActivity::class.java)
+        val extraData = BaseHelper().baseToJson(data)
+        intent.putExtra(extraKey, extraData)
+        this.startActivity(intent)
+    }
+
+}
